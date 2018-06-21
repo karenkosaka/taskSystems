737e069ab509da9c57983c0104d4de84ff25975b
第二技術開発部　高坂　夏怜 <karen-kosaka@dac.co.jp>
2018-06-21 14:32:33 +0900

    DB登録追加

--- task_systems/manager/models/button.py
+++ task_systems/manager/models/button.py
@@ -1,5 +1,22 @@
 #-*- coding:utf-8 -*-
 from django.db import models
+import sqlite3
 
-
-"日付ボタンが押された時に登録画面の表示"
+class create_gui():
+    "タスク登録画面"
+    entry1 = tk.Entry(root,font=("",20),justify="center",width=25)
+    entry1.pack()
+    
+class create_sql():
+    "DBにタスクを登録"
+    def __init__(self):
+        db = sqlite3.connect("data.db")
+        cr = db.cursor()
+        #タスクの読み込み
+        task = entry1.get()
+        try:
+            cr.execute('create table main_table(task)')
+            print("１件登録しました")
+        except:
+            print("エラーにより登録できませんでした")
+            
